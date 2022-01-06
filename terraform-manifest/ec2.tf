09f2407340f8209f1d888ca1d45cd251e0582ec8
root <root@ip-172-31-32-47.ap-south-1.compute.internal>
2022-01-06 09:32:07 +0000

    goldrhkdddd

--- terraform-manifest/ec2.tf
+++ terraform-manifest/ec2.tf
@@ -1,18 +1,5 @@
-# Terraform Settings Block
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      #version = "~> 3.21" # Optional but recommended in production
-    }
-  }
-}
 
-# Provider Block
-provider "aws" {
- # profile = "default" # AWS Credentials Profile configured on your local desktop terminal  $HOME/.aws/credentials
-  region  = "ap-south-1"
-}
+
 
 module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
